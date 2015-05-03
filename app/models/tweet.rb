a32a68bc19477b116383751d89401b0d100ded7d
Ryoma ABE <arayahomuhomu@gmail.com>
2015-05-03 11:46:44 +0900

    バリデーションを記載

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,3 +1,7 @@
 class Tweet < ActiveRecord::Base
+  validates :url, presence: true
+  validates :fav, presence: true
+  validates :rt, presence: true
+  validates :status_id, presence: true
   belongs_to :twitter_account
 end
