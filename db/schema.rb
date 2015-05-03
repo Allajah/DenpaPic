809d3fd346f8d509163930ba2f53107a98ed89c5
Ryoma ABE <arayahomuhomu@gmail.com>
2015-05-03 11:53:23 +0900

    picturesテーブルをマイグレーション

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150503023508) do
+ActiveRecord::Schema.define(version: 20150503024950) do
 
   create_table "denpas", force: :cascade do |t|
     t.string   "name",       limit: 255, null: false
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
   end
+
+  create_table "pictures", force: :cascade do |t|
+    t.string   "url",        limit: 255, null: false
+    t.integer  "tweet_id",   limit: 4
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
+  add_index "pictures", ["tweet_id"], name: "index_pictures_on_tweet_id", using: :btree
 
   create_table "tweets", force: :cascade do |t|
     t.string   "url",                limit: 255, null: false
@@ -41,5 +50,6 @@ ActiveRecord::Schema.define(version: 20150503023508) do
 
   add_index "twitter_accounts", ["denpa_id"], name: "index_twitter_accounts_on_denpa_id", using: :btree
 
+  add_foreign_key "pictures", "tweets"
   add_foreign_key "tweets", "twitter_accounts"
 end
